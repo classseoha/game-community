46ae10d350e7c6ecd4f4fc9c0f9a4910cac8ca8f
Jess <jcscene@gmail.com>
2025-05-21 16:03:48 +0900

    test: PostEntityFetcherTest_정상 반환 테스트 추가

--- src/test/java/com/example/gamecommunity/common/util/PostEntityFetcherTest.java
+++ src/test/java/com/example/gamecommunity/common/util/PostEntityFetcherTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.gamecommunity.common.exception.CustomException;
+import com.example.gamecommunity.domain.post.entity.Post;
 import com.example.gamecommunity.domain.post.repository.PostRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,10 +34,26 @@ class PostEntityFetcherTest {
 		when(postRepository.findById(id)).thenReturn(Optional.empty());
 
 		// when & then
-		assertThatThrownBy(() -> entityFetcher.getPostOrThrow(id))
+		assertThatThrownBy(() -> entityFetcher.getPostOrThrow(id)) // assertThatThrownBy : 예외 검증
 			.isInstanceOf(CustomException.class)
+			// 예외 메시지 검증
 			.hasMessageContaining("게시글을 찾을 수 없습니다.");
 
+	}
+
+	@Test
+	void 일치하는_게시글id_있으면_정상_반환() {
+
+		// given
+		Long id = 1L;
+		Post mockPost =  mock(Post.class);
+		when(postRepository.findById(id)).thenReturn(Optional.of(mockPost));
+
+		// when
+		Post result = entityFetcher.getPostOrThrow(id);
+
+		// then
+		assertThat(result).isEqualTo(mockPost);
 	}
 
 }
---
+++ src/test/java/com/example/gamecommunity/domain/post/entity/UpdatePostInfoTest.java
@@ -0,0 +1,12 @@
+package com.example.gamecommunity.domain.post.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UpdatePostInfoTest {
+
+	@Test
+	void updatePostInfo() {
+	}
+}
