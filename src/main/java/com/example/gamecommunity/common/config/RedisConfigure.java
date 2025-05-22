31c9cb6944473aef65065df18793b5f1ac9e4add
gganb <gksql0801@naver.com>
2025-05-22 10:40:27 +0900

    fix : 비밀번호 필드 제거

--- src/main/java/com/example/gamecommunity/common/config/RedisConfigure.java
+++ src/main/java/com/example/gamecommunity/common/config/RedisConfigure.java
@@ -3,6 +3,7 @@ package com.example.gamecommunity.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +23,10 @@ public class RedisConfigure {
     @Value("${spring.data.redis.port}")
     private int port;   // port 정보 가져오기
 
-    @Value("${spring.data.redis.password}")
-    private String password;    // 비밀번호 가져오기
+    @PostConstruct
+    public void printRedisConnection() {
+        System.out.println("현재 Redis 연결: " + host + ":" + port);
+    }
 
     // 백엔드, redis 연결 - Lettuce 사용
     @Bean
@@ -31,7 +34,6 @@ public class RedisConfigure {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
@@ -54,47 +56,6 @@ public class RedisConfigure {
         // GenericJackson2JsonRedisSerializer 사용해 객체를 JSON으로 직렬화해 저장함
         redisTemplate.setValueSerializer(serializer);
         return redisTemplate;
-//    /**
-//     * RedisTemplate 설정
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplateObject(ObjectMapper redisObjectMapper) {
-//        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
-//        // 데이터 저장, 조회를 위한 핵심 객체
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        // Redis 서버와의 연결 정보 설정, redisConnectionFactory() -> LettuceConnectionFactory 반환
-//        redisTemplate.setConnectionFactory(redisConnectionFactory());
-//        // Redis에 데이터 저장시 어떻게 직렬화할지 지정.
-//        // key를  문자열(utf-8 바이트 배열)로 직렬화 함
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        // GenericJackson2JsonRedisSerializer 사용해 객체를 JSON으로 저장함
-////        redisTemplate.setValueSerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(serializer);
-//        return redisTemplate;
-//    }
 
-//    /**
-//     * RedisTemplate 설정
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public RedisTemplate<String, HotPostResponseDto> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
-//        // objectMapper 를 GenericJackson2JsonRedisSerializer에 적용
-//        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-//
-//        // 데이터 저장, 조회를 위한 핵심 객체
-//        RedisTemplate<String, HotPostResponseDto> redisTemplate = new RedisTemplate<>();
-//        // Redis 서버와의 연결 정보 설정, redisConnectionFactory() -> LettuceConnectionFactory 반환
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        // Redis에 데이터 저장시 어떻게 직렬화할지 지정.
-//        // key를  문자열(utf-8 바이트 배열)로 직렬화 함
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        // GenericJackson2JsonRedisSerializer 사용해 객체를 JSON으로 직렬화해 저장함
-//        redisTemplate.setValueSerializer(serializer);
-//        return redisTemplate;
-//    }
     }
 }
