2f3c638a4a84794d13c088d8654abb0d7277dda9
gganb <gksql0801@naver.com>
2025-05-23 10:28:39 +0900

    docs : 주석 추가

--- src/main/java/com/example/gamecommunity/domain/post/service/CacheEvictionService.java
+++ src/main/java/com/example/gamecommunity/domain/post/service/CacheEvictionService.java
@@ -31,7 +31,8 @@ public class CacheEvictionService {
     }
 
     /**
-     * unlink 명령 이용해 키 삭제
+     * UNLINK 명령 이용해 키 삭제
+     * @param keys 삭제할 키의 집합
      */
     private void unlinkKeys(Set<String> keys) {
         redisTemplate.execute((RedisCallback<Void>) connection -> {
@@ -42,16 +43,24 @@ public class CacheEvictionService {
         });
     }
 
+    /**
+     * SCAN 명령 사용해 키 조회
+     * @param keyPattern 조회 키 패턴 (ex) "search:result:*"
+     * @return 검색한 키들의 Set<String>
+     */
     private Set<String> scanKeys(String keyPattern) {
         // RedisCallback : RedisConnectrion을 직접 제어할 수 있는 low-level API
         return redisTemplate.execute((RedisCallback<Set<String>>) connection -> {
+            // 결과 저장용 Set
             Set<String> keys = new HashSet<>();
 
+            // 옵션 설정 : 키 패턴, 반환 개수 (기본값 10)
             ScanOptions options = ScanOptions.scanOptions()
                     .match(keyPattern)   // key 패턴
-                    .count(10)  // 기본값. 한번에 가져올 키 수
+                    .count(10)  // 한번에 가져올 키 수
                     .build();
 
+            // 커서 기반 키 조회
             Cursor<byte[]> cursor = connection.scan(options);   // Cursor: 반복 가능한 객체, Redis의 키는 byte[] 형식
 
             while (cursor.hasNext()) {
